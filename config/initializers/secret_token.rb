c91b3584fd302a5f7d687895e8bbe13dd2e773b7
Alexey Ivanov <Alexey.Ivanov@bfoxint.com>
2016-02-06 10:15:18 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bowerrr::Application.config.secret_token = '5c8f31febce62fb39b51bbb3e5721b7761731fab42f953ffc4b6780e62864eb1e30e5ca8ed64ea93dced8dd05a9f120d269de72137525b4eb33c720a79de7361'
